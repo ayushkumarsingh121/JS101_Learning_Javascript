7045d54640ac646071b8444c05d6849cb6428c37
ayushkumarsingh121 <ayushkumarsinghrajput121@gmail.com>
2024-02-14 19:12:34 +0530

    IwMathmaticalOperatior

---
+++ MathmaticalOperatior.js
@@ -0,0 +1,9 @@
+function squareAndSum(one, two, three) {
+  // Write code here
+//   console.log((one*one)+(two*two)+(three*three))
+
+    let value = (one*one) + (two*two) + (three*three)
+    
+    console.log(value);    
+    
+}
